17aab2dd7be43960b040efc3f933c49d949a7f43
Steven <bonillasc@gmail.com>
2018-11-01 16:14:40 -0400

    restructuring

---
+++ source/CommandLine.java
@@ -0,0 +1,10 @@
+package source;
+
+//import source.CheckPlagiarism;
+
+public class CommandLine{
+
+    public static void main(String[] args){
+        CheckPlagiarism cp = new CheckPlagiarism();
+    }
+}
