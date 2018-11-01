15ed6223b96b7b57d43ea49248b2890aa148d8d0
Steven <bonillasc@gmail.com>
2018-11-01 19:08:16 -0400

    added commandline class, to make documentation and minor refactoring

--- source/CommandLine.java
+++ source/CommandLine.java
@@ -1,10 +1,42 @@
 package source;
 
-//import source.CheckPlagiarism;
+import java.util.Arrays;
 
 public class CommandLine{
 
+    public final String INPUT_ERR = "Input error. Exiting.";
+
     public static void main(String[] args){
-        CheckPlagiarism cp = new CheckPlagiarism();
+        new CommandLine().readCommandLine(args);
     }
+
+    public void readCommandLine(String[] args){
+        if (args == null){ System.err.println(INPUT_ERR); return; }
+
+        // command line arguments
+        String synonymsFile = "";
+        String file1 = "";
+        String file2 = "";
+        int tupleSize = 3;
+
+        if (args.length == 3 || args.length == 4){
+             try{
+                synonymsFile = args[0].trim();
+                file1 = args[1].trim();
+                file2 = args[2].trim();
+
+                if (args.length == 4)
+                    tupleSize = Integer.valueOf(args[3]);
+            }
+            catch(Exception e){
+                System.err.println(INPUT_ERR);
+                e.printStackTrace();
+            }
+        } else
+            System.err.println(INPUT_ERR);
+
+        new CheckPlagiarism().checkPlagiarism(synonymsFile, file1, file2, tupleSize);
+    }
+
+
 }
